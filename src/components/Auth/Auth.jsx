0ae1976e83e6d9540c38a3fe778e2f1cfd057816
Jeancarlo Morales <jcode2006@gmail.com>
2023-07-05 19:38:45 -0500

    feat: :sparkles: add auth component
    use validations and create signin and signup functions

---
+++ src/components/Auth/Auth.jsx
@@ -0,0 +1,314 @@
+'use client'
+
+import { useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { toast } from 'react-hot-toast'
+
+const Auth = ({ providers }) => {
+  const supabaseClient = useSupabaseClient()
+
+  const router = useRouter()
+
+  const [isLogin, setIsLogin] = useState(true)
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState()
+  const [errorName, setErrorName] = useState()
+  const [errorEmail, setErrorEmail] = useState()
+  const [errorPassword, setErrorPassword] = useState()
+
+  const handleSignIn = async () => {
+    if (!name || !email || !password) {
+      setError('Please fill in all fields')
+      setTimeout(() => {
+        setError(null)
+      }, 5000) // reset error after 5 seconds
+
+      // focus on first empty input field
+      if (!name) {
+        document.getElementById('name').focus()
+        return
+      }
+      if (!email) {
+        document.getElementById('email').focus()
+        return
+      }
+      if (!password) {
+        document.getElementById('password').focus()
+        return
+      }
+      return
+    }
+    const nameRegex = /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/
+    const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+
+    if (!nameRegex.test(name)) {
+      setErrorName('Please enter a valid name')
+      setTimeout(() => {
+        setErrorName(null)
+      }, 5000) // reset error after 5 seconds
+      return
+    }
+
+    if (!emailRegex.test(email)) {
+      setErrorEmail('Please enter a valid email address')
+      setTimeout(() => {
+        setErrorEmail(null)
+      }, 5000) // reset error after 5 seconds
+      return
+    }
+
+    if (password.length < 8 || password.length > 32) {
+      setErrorPassword('Password must be between 8 and 32 characters')
+      setTimeout(() => {
+        setErrorPassword(null)
+      }, 5000) // reset error after 5 seconds
+      return
+    }
+
+    if (!passwordRegex.test(password)) {
+      setErrorPassword(
+        'Password must contain at least one uppercase letter, one lowercase letter and one number'
+      )
+      setTimeout(() => {
+        setErrorPassword(null)
+      }, 5000)
+      return
+    }
+
+    const { error } = await supabaseClient.auth.signUp({
+      email,
+      password,
+      options: {
+        data: { name }
+      }
+    })
+
+    if (error) setError(error.message)
+    else {
+      toast.success('Account created successfully!')
+      router.refresh()
+    }
+  }
+
+  const handleSignUp = async () => {
+    if (!email || !password) {
+      setError('Please fill in all fields')
+      setTimeout(() => {
+        setError(null)
+      }, 5000) // reset error after 5 seconds
+
+      // focus on first empty input field
+      if (!email) {
+        document.getElementById('email').focus()
+        return
+      }
+      if (!password) {
+        document.getElementById('password').focus()
+        return
+      }
+      return
+    }
+
+    const emailRegex = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/
+
+    if (!emailRegex.test(email)) {
+      setErrorEmail('Please enter a valid email address')
+      setTimeout(() => {
+        setErrorEmail(null)
+      }, 5000) // reset error after 5 seconds
+      return
+    }
+
+    if (password.length < 8 || password.length > 32) {
+      setErrorPassword('Password must be between 8 and 32 characters')
+      setTimeout(() => {
+        setErrorPassword(null)
+      }, 5000) // reset error after 5 seconds
+      return
+    }
+
+    if (!passwordRegex.test(password)) {
+      setErrorPassword(
+        'Password must contain at least one uppercase letter, one lowercase letter and one number'
+      )
+      setTimeout(() => {
+        setErrorPassword(null)
+      }, 5000)
+      return
+    }
+
+    const { error } = await supabaseClient.auth.signInWithPassword({
+      email,
+      password
+    })
+
+    if (error) setError(error.message)
+    else {
+      toast.success('Logged in successfully!')
+      router.refresh()
+    }
+  }
+
+  return (
+    <>
+      <h3 className='text-center text-2xl font-bold text-neutral-900'>
+        {isLogin ? 'Crea una cuenta' : 'Bienvenido de nuevo'}
+      </h3>
+
+      <p className='mb-2 text-center text-neutral-500'>
+        {isLogin ? 'Regístrese para continuar' : 'Inicia sesión para continuar'}
+      </p>
+
+      <form
+        className='flex w-full flex-col items-center justify-center gap-6 px-4'
+        onSubmit={(e) => {
+          e.preventDefault()
+          setError(null)
+          isLogin ? handleSignIn(e) : handleSignUp(e)
+        }}
+      >
+        <div className='flex w-full flex-col items-center justify-center gap-4'>
+          <button
+            type='button'
+            onClick={() => {}}
+            className='flex w-full items-center justify-center gap-2 rounded-md bg-neutral-200/75 px-4 py-2 text-neutral-900'
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 512 512'
+              height='24'
+              width='24'
+            >
+              <path
+                fill='#4285f4'
+                d='M386 400c45-42 65-112 53-179H260v74h102c-4 24-18 44-38 57z'
+              />
+              <path
+                fill='#34a853'
+                d='M90 341a192 192 0 0 0 296 59l-62-48c-53 35-141 22-171-60z'
+              />
+              <path
+                fill='#fbbc02'
+                d='M153 292c-8-25-8-48 0-73l-63-49c-23 46-30 111 0 171z'
+              />
+              <path
+                fill='#ea4335'
+                d='M153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55z'
+              />
+            </svg>
+            <span className='text-base font-medium'>
+              Inicia sesión con Google
+            </span>
+          </button>
+        </div>
+
+        <hr className='w-full border border-neutral-700/10' />
+
+        {isLogin && (
+          <div className='flex w-full flex-col gap-2'>
+            <label
+              htmlFor='name'
+              className='cursor-pointer text-base font-medium text-neutral-700'
+            >
+              Name
+            </label>
+            <input
+              className='h-10 w-full rounded-sm border border-neutral-700/10 bg-neutral-50 px-2 text-sm font-semibold outline-none ring-2 ring-transparent placeholder:text-black placeholder:text-opacity-50 focus:ring-1 focus:ring-neutral-300'
+              type='text'
+              id='name'
+              placeholder='Enter your name'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+
+            {error && <p className='text-sm text-red-500'>{error}</p>}
+            {errorName && <p className='text-sm text-red-500'>{errorName}</p>}
+          </div>
+        )}
+
+        <div className='flex w-full flex-col gap-2'>
+          <label
+            htmlFor='email'
+            className='cursor-pointer text-base font-medium text-neutral-700'
+          >
+            Email
+          </label>
+          <input
+            className='h-10 w-full rounded-sm border border-neutral-700/10 bg-neutral-50 px-2 text-sm font-semibold outline-none ring-2 ring-transparent placeholder:text-black placeholder:text-opacity-50 focus:ring-1 focus:ring-neutral-300'
+            type='email'
+            id='email'
+            placeholder='Enter your email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoFocus
+          />
+
+          {error && <p className='text-sm text-red-500'>{error}</p>}
+          {errorEmail && <p className='text-sm text-red-500'>{errorEmail}</p>}
+        </div>
+
+        <div className='flex w-full flex-col gap-2'>
+          <label
+            htmlFor='password'
+            className='cursor-pointer text-base font-medium text-neutral-700'
+          >
+            Password
+          </label>
+          <input
+            className='h-10 w-full rounded-sm border border-neutral-700/10 bg-neutral-50 px-2 text-sm font-semibold outline-none ring-2 ring-transparent placeholder:text-black placeholder:text-opacity-50 focus:ring-1 focus:ring-neutral-300'
+            type='password'
+            id='password'
+            placeholder='Enter your password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          {error && <p className='text-sm text-red-500'>{error}</p>}
+          {errorPassword && (
+            <p className='text-sm text-red-500'>{errorPassword}</p>
+          )}
+        </div>
+
+        <div className='flex w-full flex-col items-center justify-center gap-4'>
+          <button
+            type='submit'
+            className='flex w-full items-center justify-center gap-2 rounded-md bg-gradient-to-r from-orange-500 to-red-500 px-4 py-2 text-white'
+          >
+            <span className='text-base font-medium'>
+              {isLogin ? 'Regístrate' : 'Inicia sesión'}
+            </span>
+          </button>
+        </div>
+
+        {!isLogin ? (
+          <p className='text-sm font-medium text-neutral-700'>
+            ¿No tienes una cuenta?{' '}
+            <span
+              className='cursor-pointer text-sm font-semibold text-orange-500'
+              onClick={() => setIsLogin(!isLogin)}
+            >
+              Regístrate
+            </span>
+          </p>
+        ) : (
+          <p className='text-sm font-medium text-neutral-700'>
+            ¿Ya tienes una cuenta?{' '}
+            <span
+              className='cursor-pointer text-sm font-semibold text-orange-500'
+              onClick={() => setIsLogin(!isLogin)}
+            >
+              Inicia sesión
+            </span>
+          </p>
+        )}
+      </form>
+    </>
+  )
+}
+
+export default Auth
