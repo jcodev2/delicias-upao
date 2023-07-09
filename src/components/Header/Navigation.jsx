233d5a6ed0b50323e77d7c08c2a2aa6f7d0d8338
Jeancarlo Morales <jcode2006@gmail.com>
2023-07-09 18:39:26 -0500

    fix: :lipstick: update navigation mobile icon color hover bug

--- src/components/Header/Navigation.jsx
+++ src/components/Header/Navigation.jsx
@@ -13,9 +13,9 @@ const Navigation = () => {
         {routes.map(({ name, Icon, path }) => (
           <li
             key={name}
-            className={`relative flex w-full cursor-pointer flex-col items-center justify-center rounded-full bg-gradient-to-r py-2 font-medium transition hover:text-neutral-100 md:rounded-none md:py-0 ${
+            className={`relative flex w-full cursor-pointer flex-col items-center justify-center rounded-full bg-gradient-to-r py-2 font-medium transition hover:text-neutral-600 md:rounded-none md:py-0 ${
               router === path
-                ? 'bg-gradient-to-r from-orange-500 to-red-500 text-neutral-100 hover:text-neutral-100 md:bg-transparent md:from-transparent md:to-transparent md:text-black md:hover:text-neutral-600'
+                ? 'bg-gradient-to-r from-orange-500 to-red-500 md:bg-transparent md:from-transparent md:to-transparent md:text-black md:hover:text-neutral-600'
                 : ''
             }`}
           >
@@ -23,7 +23,11 @@ const Navigation = () => {
               href={path}
               className='inline-flex h-full w-full flex-col items-center justify-center'
             >
-              <Icon className='text-2xl md:hidden' />
+              <Icon
+                className={`text-2xl md:hidden ${
+                  router === path ? 'text-neutral-100' : ''
+                }`}
+              />
               <span className='hidden md:block'>{name}</span>
             </Link>
 
