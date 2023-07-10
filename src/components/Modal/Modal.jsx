bc004f7276679f2de59cc77dcea7c72cef3796d0
jcodev2 <jcode2006@gmail.com>
2023-07-10 05:49:59 -0500

    refactor: :lipstick: update padding top on modal for mobile responsive

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -10,10 +10,10 @@ const Modal = ({ isOpen, onChange, children }) => {
         <section className='fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center'>
           <Overlay onChange={onChange} />
 
-          <article className='relative flex min-h-screen w-full max-w-3xl flex-col justify-start bg-white p-4 shadow-lg md:min-h-min md:flex-row md:gap-0 md:rounded-lg md:p-0 md:shadow-xl md:backdrop-blur-lg'>
+          <article className='relative flex min-h-screen w-full max-w-3xl flex-col justify-start bg-white p-4 pt-10 shadow-lg md:min-h-min md:flex-row md:gap-0 md:rounded-lg md:p-0 md:shadow-xl md:backdrop-blur-lg'>
             <div className='flex w-full flex-col items-center justify-start gap-2 md:w-1/2 md:items-center md:justify-center md:py-8'>
               <button
-                className='absolute right-4 top-4 text-3xl text-neutral-500 transition hover:text-neutral-600 md:right-2 md:top-2 md:text-neutral-100 md:hover:text-neutral-300'
+                className='absolute right-4 top-10 text-3xl text-neutral-500 transition hover:text-neutral-600 md:right-2 md:top-2 md:text-neutral-100 md:hover:text-neutral-300'
                 onClick={() => onChange(false)}
               >
                 <IoMdClose />
